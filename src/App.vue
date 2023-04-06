5cc0c9abf111dac60be90a3f1323d6f7d73a1c6f
Peter Van Esch <pvanesch@ucsc.edu>
2023-04-05 19:25:18 -0700

    orange pic

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Home msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,12 +17,4 @@ export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
