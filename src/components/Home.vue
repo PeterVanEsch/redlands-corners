47739d2bde0e049fed1ce121d44c5d71e2d3cc16
Ryan Thomas King <rytking@ucsc.edu>
2023-04-05 21:41:13 -0700

    day calculator

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,20 @@ import { Options, Vue } from 'vue-class-component';
 @Options({
 })
 export default class Home extends Vue {
+  private allStreets = ['1st', 'Gavin', 'Maple'];
+  private street1 = '';
+  private street2 = '';
 
+  getDate() {
+    return new Date();
+  }
+
+  getDayOfYear(date: Date) {
+    let start = new Date(date.getFullYear(), 0, 0);
+    let diff = date.getTime() - start.getTime() + (start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000;
+    let day = Math.floor(diff / 86400000);
+    return day;
+  }
 }
 </script>
 
